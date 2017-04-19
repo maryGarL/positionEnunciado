b1730bdf1abffbc479cc52700915b899da132637
maryGarL <maryel_565@hotmail.com>
2017-04-19 11:03:44 -0500

    ejercicio 1 terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 var body=document.getElementsByTagName("BODY")[0]
+var contenedor=document.createElement("div");
 for (var i = 1; i < 3; i++) {
 var div0=document.createElement("div");
 var div1=document.createElement("div");
@@ -6,7 +7,9 @@ var div2=document.createElement("div");
 div0.setAttribute("id","divPrincipal-"+i);
 div1.setAttribute("id","divHijo")
 div2.setAttribute("id","divNieto")
+contenedor.setAttribute("id","cajapadre")
 div1.appendChild(div2);
 div0.appendChild(div1);
-body.appendChild(div0);
+contenedor.appendChild(div0);
+body.appendChild(contenedor)
 }
